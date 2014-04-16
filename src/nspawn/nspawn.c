2ea68ac0cf59ef61a1fe24845bcc9055a86788da
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-16 10:59:28 +0200

    Fix NixOS containers
    In NixOS containers, the init script is bind-mounted into the
    container, so checking early whether it exists will fail.

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -3699,6 +3699,7 @@ int main(int argc, char *argv[]) {
                                 goto finish;
                         }
                 } else {
+#if 0
                         const char *p;
 
                         p = strjoina(arg_directory, "/usr/");
@@ -3707,6 +3708,7 @@ int main(int argc, char *argv[]) {
                                 r = -EINVAL;
                                 goto finish;
                         }
+#endif
                 }
 
         } else {
